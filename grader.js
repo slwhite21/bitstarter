d815abe80a73795e1e75bf010a71e1ee091f85e9
Ubuntu <ubuntu@ip-172-31-23-10.us-west-2.compute.internal>
2013-07-11 02:23:43 +0000

    Added grader.js and json

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://calm-hamlet-9158.herokuapp.com/";
+var filename = "web.html"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +58,15 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var urlToHtmlFile = function(urlpath) {
+  rest.get(urlpath).on('complete', function(result) {
+    fs.writeFileSync(filename, result);
+    //console.log(result);
+    return filename;
+  });
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +77,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_path>', 'URL path', clone(urlToHtmlFile), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkHtmlJson = checkHtmlFile(program.file, program.checks);
+    var outHtmlJson = JSON.stringify(checkHtmlJson, null, 4);
+    //console.log(outHtmlJson);
+    var checkUrlJson = checkHtmlFile(filename, program.checks);
+    var outUrlJson = JSON.stringify(checkUrlJson, null, 4);
+    console.log(outUrlJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
